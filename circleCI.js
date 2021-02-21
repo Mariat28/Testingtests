ff3b503e8bbabddec1080cf550905887c2a4ee3f
Mariat Ndagire <mariatndagire28@gmail.com>
2021-02-21 19:46:34 +0300

    Integrating github with CircleCI initial commit

---
+++ circleCI.js
@@ -0,0 +1,19 @@
+let x = 9;
+let y = 20;
+z = x + y;
+
+function add() {
+    if (z < 10) {
+        console.log("You gat that wrong");
+
+    } else {
+        console.log("Yaaay! You're a genius");
+    }
+    return z;
+
+}
+let results = add()
+module.exports = results;
+
+
+
