7578ca863be0e1c1c8c3aab71ddf23c6f1df89d2
Mariat Ndagire <mariatndagire28@gmail.com>
2021-02-23 22:04:13 +0300

    result less than 10

--- circleCI.js
+++ circleCI.js
@@ -1,14 +1,8 @@
 function add(x,y) {
-    // if (z < 10) {
-    //     console.log("You gat that wrong");
-
-    // } else {
-    //     console.log("Yaaay! You're a genius");
-    // }
     return x+y;
 
 }
-let results = add(9,20)
+let results = add(5,0)
 module.exports = results;
 
 
